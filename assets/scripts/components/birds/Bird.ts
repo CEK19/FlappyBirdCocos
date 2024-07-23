c94413c4a3f196634d0552e6b7bf5967cf47e316
CEK19 <nguyentrongnhan2001.work@gmail.com>
2024-07-23 14:18:32 +0700

    [flappy bird] update feeling of playing bird

--- assets/scripts/components/birds/Bird.ts
+++ assets/scripts/components/birds/Bird.ts
@@ -1,9 +1,17 @@
-import { _decorator, Component, Input, input, RigidBody2D, Vec2 } from "cc";
+import {
+	_decorator,
+	Component,
+	Input,
+	input,
+	Quat,
+	RigidBody2D,
+	Vec2,
+} from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("Bird")
 export class Bird extends Component {
-	private JUMP_DURATION: number = 0.1;
+	private JUMP_DURATION: number = 0.2;
 
 	private _jumpTime: number = 0;
 	private _isJumping: boolean = false;
@@ -18,24 +26,40 @@ export class Bird extends Component {
 	}
 
 	private jump() {
+		this._isJumping = true;
+		this._rigidBody.linearVelocity = new Vec2(0, 10);
+		this._jumpTime = 0;
+
 		if (this._isJumping) {
 			return;
 		}
-
-		this._isJumping = true;
-		this._rigidBody.linearVelocity = new Vec2(0, 30);
 	}
 
 	update(deltaTime: number) {
+		if (this._rigidBody.linearVelocity.y < 0) {
+			this.rotateBirdDown();
+		} else {
+			this.rotateBirdNormal();
+		}
+
 		if (!this._isJumping) {
 			return;
 		}
 
 		this._jumpTime += deltaTime;
+		this;
 		if (this._jumpTime >= this.JUMP_DURATION) {
 			this._jumpTime = 0;
 			this._isJumping = false;
 			this._rigidBody.linearVelocity = new Vec2(0, -10);
 		}
+	}
+
+	private rotateBirdDown() {
+		this.node.setRotation(Quat.fromEuler(new Quat(), 0, 0, -20));
+	}
+
+	private rotateBirdNormal() {
+		this.node.setRotation(Quat.fromEuler(new Quat(), 0, 0, 0));
 	}
 }
